8484d29cbad567f75b8641ba1e4778cf0a3f905d
Pewww <jung22337@naver.com>
2020-06-13 16:45:20 +0900

    :hammer: Lazy Loading 테스트를 위해 큰 이미지의 파일들 상수 파일로 분리

---
+++ progressive-image/src/constants/images.ts
@@ -0,0 +1,14 @@
+const images = [
+  'https://wallpaperaccess.com/full/817028.jpg',
+  'https://wallpapercave.com/wp/wp4469520.jpg',
+  'https://wallpapersmug.com/download/3840x2160/e01942/cybercity-art-streets.jpg',
+  'https://wallpaperaccess.com/full/235700.jpg',
+  'https://i.redd.it/8ncj88qz0evy.jpg',
+  'https://wallpaperaccess.com/full/1271514.jpg',
+  'https://external-preview.redd.it/GOkP8onbuyjGmN9Rc8Que5mw21CdSw6OuXpAKUuE6-4.jpg?auto=webp&s=2bc0e522d1f2fa887333286d557466b2be00fa5e',
+  'https://images.wallpapersden.com/image/download/neon-wave-futuristic-city_63625_3840x2160.jpg',
+  'https://uhdwallpapers.org/uploads/converted/18/10/14/city-lights-by-asus-rog-wallpaper-3840x2160_55744-mm-90.jpg',
+  'https://i.pinimg.com/originals/c6/22/46/c62246425d2c45d07ff2ad24ecd620cc.jpg'
+];
+
+export default images;
